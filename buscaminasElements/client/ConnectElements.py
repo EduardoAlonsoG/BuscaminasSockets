38a45d721d565b805c101bc4c4e6413f95fcf5dd
EAlonso <garcia.alonso.eduardo1@gmail.com>
2024-09-29 13:39:22 -0600

    Creating the client and server packages

---
+++ buscaminasElements/client/ConnectElements.py
@@ -0,0 +1,15 @@
+class ConnectElements:
+    def __init__(self , hostIP , hostPort, matchName):
+        self._hostIP = hostIP
+        self._hostPort = hostPort
+        self._matchName = matchName
+
+    def getHostIp(self):
+        return self._hostIP
+
+    def getHostPort(self):
+        return self._hostPort
+
+    def getMatchName(self):
+        return self._matchName + " located at " + self._hostIP
+
